63e2aa26d04c3cd4726a5b9c77a990ac6b5ef122
Vlad Levishev <vlad.levishev@incredibuild.com>
2017-02-06 17:18:55 +0200

    Guess The Artist Game starts coding: JsonReader class created

---
+++ Java/GuessTheArtistGame/src/core/JSONReader.java
@@ -0,0 +1,41 @@
+package core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JSONReader {
+	
+	public String readAll(Reader rd) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while((cp=rd.read())!=-1){
+			sb.append((char) cp);
+		}
+		return sb.toString();
+	}
+	
+	public JSONObject readJsonFromURL (String url) throws MalformedURLException, IOException, JSONException{
+		
+			InputStream is = new URL(url).openStream();
+		try{	
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			String jsonText = readAll(br);
+			
+			JSONObject json = new JSONObject(jsonText);
+			
+		return json;
+		} finally {
+			is.close();
+		}
+	}
+	
+}
