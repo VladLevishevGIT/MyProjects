4f6c89795e842d99ac8570a66adcf42cdf55ae7d
Vlad Levishev - Laptop <vlad.levishev@gmail.com>
2017-04-27 21:53:45 +0200

    Some new changes

--- IntelliJ_Projects/GuessTheArtistQuiz/out/artifacts/web_war_exploded/angular/angularCode01.js
+++ IntelliJ_Projects/GuessTheArtistQuiz/out/artifacts/web_war_exploded/angular/angularCode01.js
@@ -3,7 +3,7 @@ var quizMainApp = angular.module("QuizApp", [])
 quizMainApp.controller("QuizAppCtrl", function ($http, $scope) {
     $http.get('rest/quizWebService/getFirstSong01').
     then(function(response) {
-        $scope.round = response.data
-        alert($scope.round)
+        $scope.round = response.data;
     });
+
 })
