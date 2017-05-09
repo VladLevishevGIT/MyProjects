e57c0010bd8db21d112488744ce47c0dab95683a
Vlad Levishev - Laptop <vlad.levishev@gmail.com>
2017-05-09 17:58:21 +0200

    New quiz logic

--- IntelliJ_Projects/GuessTheArtistQuiz/web/angular/angularCode01.js
+++ IntelliJ_Projects/GuessTheArtistQuiz/web/angular/angularCode01.js
@@ -1,14 +1,28 @@
+var songCount = 1;
 var quizMainApp = angular.module("QuizApp", [])
 
 quizMainApp.controller("QuizAppCtrl", function ($http, $scope) {
     $http.get('rest/quizWebService/getFirstSong01').
     then(function(response) {
-        $scope.round = response.data;
+        $scope.returnedRound = response.data;
+        $scope.firstSong = $scope.returnedRound.firstSong;
     });
 
-    $scope.testFunction = function () {
-        var test = $scope.round;
-        alert(JSON.stringify(test));
+    $scope.getOneMoreSong = function (inputValue) {
+        if(inputValue == $scope.returnedRound.artistName) {
+            $scope.showSuccessAlert = true;
+            $scope.successTextAlert = "Very Good!"
+        }else if(songCount=1) {
+            $scope.firstSong = $scope.returnedRound.firstSong;
+            $scope.secondSong = $scope.returnedRound.secondSong;
+            songCount=2;
+        }else if(songCount=2){
+            $scope.firstSong = $scope.returnedRound.firstSong;
+            $scope.secondSong = $scope.returnedRound.secondSong;
+            $scope.thirdSong = $scope.returnedRound.thirdSong;
+            songCount=3;
+        }else {
+            $scope.showErrorAlert = true;
+                }
     }
-
 })
